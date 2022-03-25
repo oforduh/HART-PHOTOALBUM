fd68e6e61e742dfb2010d4e6d7fa9fa0341bd0b7
oforduhh <oforduhh@gmail.com>
2022-03-25 10:21:34 +0100

    carosel

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,32 +2,30 @@ import React from "react";
 import styles from "./homepage.module.scss";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Slider1 from "../../assets/date1.jpeg";
-import Slider2 from "../../assets/date2.jpeg";
-import Slider3 from "../../assets/date3.jpeg";
+import Slider1 from "../../assets/d1.png";
+import Slider2 from "../../assets/d2.png";
+import Slider3 from "../../assets/d3.png";
 
 const HomePage = () => {
   return (
     <div className={styles.homePageParentDiv}>
-      <div className={styles.homePageContDiv}>
-        <Carousel
-          showArrows={true}
-          autoPlay={true}
-          infiniteLoop={true}
-          interval={3000}
-          showThumbs={false}
-        >
-          <div>
-            <img src={Slider1} alt="slider 1" />
-          </div>
-          <div>
-            <img src={Slider2} alt="slider 2" />
-          </div>
-          <div>
-            <img src={Slider3} alt="slider 3" />
-          </div>
-        </Carousel>
-      </div>
+      <Carousel
+        showArrows={true}
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={3000}
+        showThumbs={false}
+      >
+        <div className={styles.imageContainer}>
+          <img src={Slider1} alt="slider 1" />
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={Slider2} alt="slider 2" />
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={Slider3} alt="slider 3" />
+        </div>
+      </Carousel>
     </div>
   );
 };
